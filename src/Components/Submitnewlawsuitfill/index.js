51bdadb6a0872f3be8eacd9feab88b5b1f4527e3
omar mokhtar <al_fagomy22@yahoo.com>
2020-07-26 02:21:51 +0300

    add form elements

--- src/Components/Submitnewlawsuitfill/index.js
+++ src/Components/Submitnewlawsuitfill/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Row, Col } from "react-bootstrap";
+import { Form, Row, Col, Button } from "react-bootstrap";
 import Shout from "../../images/Submitnew.svg"; // Tell webpack this JS file uses this image
 import { Maintitle, ContairnerBg } from "./style.js";
 import './style.css';
@@ -21,8 +21,8 @@ export default function Submitnewlawsuitfill() {
                             <Form.Control type="input" />
                         </Col>
                         <Col md={3} sm="12" className="text-right">
-                            <Form.Label className="formlabel">الاسم</Form.Label>
-                            <Form.Control type="input" />
+                            <Form.Label className="formlabel">رقم الهوية</Form.Label>
+                            <Form.Control type="number" />
                         </Col>
 
                         <Col md={3} sm="12" className="text-right">
@@ -46,6 +46,35 @@ export default function Submitnewlawsuitfill() {
                         </Col>
 
 
+                    </Row>
+
+                    <Row>
+                        <Col md={8} sm="12" className="text-right mt-3">
+
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label className="formlabel">الموضوع</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+
+                        </Col>
+
+                        <Col md={4} sm="12" className="text-right mt-3">
+                            <Form.Label className="formlabel">الجنس</Form.Label>
+                            {['radio'].map((type) => (
+                                <div key={`inline-${type}`} className="mb-3">
+                                    <Form.Check inline label="1" type={type} id={`inline-${type}-1`} />
+                                    <Form.Check inline label="2" type={type} id={`inline-${type}-2`} />
+
+                                </div>
+                            ))}
+                        </Col>
+                    </Row>
+
+                    <Row>
+
+                        <Col className="text-right">
+                            <Button variant="primary" type="submit"> ارسال </Button>
+                        </Col>
                     </Row>
                 </Form>
             </ContairnerBg>
