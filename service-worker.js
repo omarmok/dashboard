ef82deeb9d191c35fbabc04b21c9054f989e2bec
omar mokhtar <al_fagomy22@yahoo.com>
2020-07-26 02:25:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.671c3b7819b6fec810771f6095c18387.js"
+  "/dashboard/precache-manifest.5ff2e45c80172411d6751669a2886d4b.js"
 );
 
 self.addEventListener('message', (event) => {
